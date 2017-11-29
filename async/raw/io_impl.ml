a71b75e7c690d6a765fba5ea3062df54412dd3a3
Steve Bleazard <sbleazard@janestreet.com>
2017-11-29 07:16:55 +0000

    clean up of async Io_impl

--- async/raw/io_impl.ml
+++ async/raw/io_impl.ml
@@ -7,10 +7,4 @@ let bind = Deferred.bind
 let map  = Deferred.map
 let (>>=) = Deferred.(>>=)
 let (>>|) = Deferred.(>>|)
-
-let sequencer = Throttle.Sequencer.create ~continue_on_error:true ()
-let wrap f v = Throttle.enqueue sequencer (fun () -> In_thread.run (fun () -> f v))
-
-let wrap_mt f v = In_thread.run (fun () -> f v)
-
-let gc_finalise f t = Gc.add_finalizer_exn t (fun t -> f t |> don't_wait_for)
+let wrap f v = In_thread.run (fun () -> f v)
